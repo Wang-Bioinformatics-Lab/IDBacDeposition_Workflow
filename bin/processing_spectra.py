ccd89c5c054112da16a5177df80a20efe8a1046d
Mingxun Wang <mwang87@gmail.com>
2022-12-20 09:02:48 -0800

    updating

--- bin/processing_spectra.py
+++ bin/processing_spectra.py
@@ -11,14 +11,13 @@ def main():
     parser.add_argument('input_metadata')
     parser.add_argument('input_spectra_folder')
     parser.add_argument('output_folder')
-    parser.add_argument('--output_identifier', default=str(uuid.uuid4()) + ".json")
+    parser.add_argument('--output_identifier', default=str(uuid.uuid4()))
 
     args = parser.parse_args()
 
     print(args)
 
     metadata_df = pd.read_excel(args.input_metadata)
-    print(metadata_df)
 
     all_rows = metadata_df.to_dict('records')
     for record in all_rows:
@@ -33,18 +32,27 @@ def main():
 
         if scan_or_coord == "*":
             # We'll just grab the first one
+            # TODO: Actually do the right thing here
             print("Grabbing first scan")
             first_scan = ms1_df["scan"].iloc[0]
             peaks_df = ms1_df[ms1_df["scan"] == first_scan]
             peaks_list = peaks_df.to_dict('records')
             peaks_list = [[peak["mz"], peak["i"]] for peak in peaks_list]
-            
-            #print(peaks_list)
+        else:
+            raise Exception("Need to implement getting scan")
 
         record["peaks"] = peaks_list
 
-    output_file = os.path.join(args.output_folder, args.output_identifier)
-    open(output_file, "w").write(json.dumps(all_rows))
+    # Outputting the JSON
+    output_json = os.path.join(args.output_folder, args.output_identifier + ".json")
+    open(output_json, "w").write(json.dumps(all_rows))
+
+    # Outputting the Summary 
+    summary_df = pd.DataFrame(all_rows)
+    summary_df = summary_df.drop(['peaks'], axis=1)
+
+    output_extraction_tsv = os.path.join(args.output_folder, args.output_identifier + ".tsv")
+    summary_df.to_csv(output_extraction_tsv, sep="\t", index=False)
 
 
 if __name__ == "__main__":
