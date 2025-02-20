6ab13d76380d13325d43773f3924f2282ed671f3
Michael Strobel <lidstromfan29@gmail.com>
2025-02-20 15:17:04 -0800

    Fix whitespace.

--- bin/convert_metadata.py
+++ bin/convert_metadata.py
@@ -14,6 +14,12 @@ def main():
     args = parser.parse_args()
 
     metadata_df = pd.read_excel(args.input_metadata)
+    # Replace newlines in column names with spaces
+    metadata_df.columns = metadata_df.columns.str.replace("\n", " ")
+    # Strip whitespace from column names
+    metadata_df.columns = metadata_df.columns.str.strip()
+    # Convert multiple spaces to single
+    metadata_df.columns = metadata_df.columns.str.replace(r" +", " ", regex=True)
     
     metadata_df.to_csv(args.output_metadata, index=False, sep="\t")
 
