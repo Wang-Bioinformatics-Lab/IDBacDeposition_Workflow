11ae66f9fa5d8abe1600e403915028fedb701ed8
Michael Strobel <lidstromfan29@gmail.com>
2025-08-05 09:02:17 -0700

    Updating.

--- bin/processing_spectra.py
+++ bin/processing_spectra.py
@@ -57,6 +57,68 @@ def load_data(input_filename):
 
     return ms1_df, ms2_df
 
+def load_metadata_file(metadata_path:str):
+    """
+    Reads a metadata file and converts it to a pandas dataframe. 
+    
+    Args:
+    metadata_path: str, path to the metadata file
+
+    Returns:
+    metadata_df: pd.DataFrame, metadata
+
+    Raises:
+    ValueError: if the metadata file is not a CSV, XLSX, XLS, or TSV file
+    """
+    metadata_path = str(metadata_path)
+    if metadata_path.endswith('.csv'):
+        metadata_df = pd.read_csv(metadata_path)
+    elif metadata_path.endswith('.xlsx'):
+        metadata_df = pd.read_excel(metadata_path, sheet_name=None)
+        # If it contains multiple tables, get the one named "Metadata sheet"
+        if isinstance(metadata_df, dict):
+            metadata_df = pd.read_excel(metadata_path, sheet_name=None)
+        if 'Metadata sheet' in metadata_df:
+            metadata_df = metadata_df['Metadata sheet']
+        elif 'Metadata template' in metadata_df:
+            metadata_df = metadata_df['Metadata template']
+        else:
+            # Pop "instructions" or Instructions or anything like that
+            all_keys = list(metadata_df.keys())
+            for key in all_keys:
+                if key.lower().strip() in ['instructions', 'instruction', 'metadata instructions']:
+                    metadata_df.pop(key)
+            # If there is only one sheet, use that
+            if len(metadata_df) == 1:
+                metadata_df = list(metadata_df.values())[0]
+            else:
+                raise ValueError(f"Excel file should contain only one sheet, or one named 'Metadata sheet' or 'Metadata template'. Instead found {metadata_df.keys()}")
+    elif metadata_path.endswith('.xls'):
+        metadata_df = pd.read_excel(metadata_path, sheet_name=None)
+        # If it contains multiple tables, get the one named "Metadata sheet"
+        if isinstance(metadata_df, dict):
+            metadata_df = pd.read_excel(metadata_path, sheet_name=None)
+        if 'Metadata sheet' in metadata_df:
+            metadata_df = metadata_df['Metadata sheet']
+        elif 'Metadata template' in metadata_df:
+            metadata_df = metadata_df['Metadata template']
+        else:
+            all_keys = list(metadata_df.keys())
+            for key in all_keys:
+                if key.lower().strip() in ['instructions', 'instruction', 'metadata instructions']:
+                    metadata_df.pop(key)
+            # If there is only one sheet, use that
+            if len(metadata_df) == 1:
+                metadata_df = list(metadata_df.values())[0]
+            else:
+                raise ValueError(f"Excel file should contain only one sheet, or one named 'Metadata sheet' or 'Metadata template'. Instead found {metadata_df.keys()}")
+    elif metadata_path.endswith('.tsv'):
+        metadata_df = pd.read_csv(metadata_path, sep='\t')
+    else:
+        raise ValueError(f'Metadata file must be a CSV, XLSX, XLS, or TSV file, but got {metadata_path} instead.')
+    
+    return metadata_df
+
 def main():
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('input_metadata')
@@ -68,23 +130,25 @@ def main():
 
     print(args)
 
-    metadata_df = pd.read_excel(args.input_metadata)
+    metadata_df = load_metadata_file(args.input_metadata)
     metadata_df.columns = metadata_df.columns.str.strip()
 
     # Make sure scan/coordinate is present
     if not "Scan/Coordinate" in metadata_df.columns:
         raise ValueError("Scan/Coordinate column not found in metadata")
-    # Ensure none are empty
-    if metadata_df["Scan/Coordinate"].isnull().values.any():
-        raise ValueError("Scan/Coordinate column has empty values")
-
-    all_rows = metadata_df.to_dict('records')
 
     columns_needed = ["Filename", "Scan/Coordinate"]
     for column in columns_needed:
         if not column in metadata_df.columns:
             print("Error, missing column", column)
             sys.exit(1)
+
+    # Ensure none are empty
+    if metadata_df["Scan/Coordinate"].isnull().values.any():
+        print("Scan/Coordinate column has empty values")
+        sys.exit(1)
+
+    all_rows = metadata_df.to_dict('records')
 
     # TODO: We should limit the column names
     columns_possible = ["Filename", "Scan/Coordinate", "Strain name"]
